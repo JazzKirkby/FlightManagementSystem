f1773aa7aea40e0ab5826c9a239a8416a45a8b04
JazzK <jhk73@bath.ac.uk>
2025-04-15 20:34:43 +0000

    copying the files before i change the contents

---
+++ flight_destination_summary.py
@@ -0,0 +1,45 @@
+# flight_destination_summary.py
+
+# Here I will define the function called flight_destination_summary
+# and create a cursor object to run the SQL queries
+def flight_destination_summary(conn):
+    cursor = conn.cursor()
+
+# User input section
+    airport_code = input("Enter Airport Code (or press return to skip): ").strip().upper()
+    status = input("Enter Flight Status (or press return to skip): ").strip().title()
+    departure_date = input("Enter Departure Date (YYYY-MM-DD) (or press return to skip): ").strip()
+
+# Query that will gather information from Flights and Destinations
+    query = """
+    SELECT Flights.departure_time, Flights.status, Destinations.airport_code
+    FROM Flights
+    JOIN Destinations ON Flights.destination_id = Destinations.destination_id
+    """
+
+# This holds the values to put into the query
+    params = []
+
+# If the user added an airport code, status or departure date, we 
+# can add this to the query and save the value to params
+    if airport_code:
+        query += " AND Destinations.airport_code = ?"
+        params.append(airport_code)
+    if status:
+        query += " AND Flights.status = ?"
+        params.append(status)
+    if departure_date:
+        query += " AND date(Flights.departure_time) = ?"
+        params.append(departure_date)
+
+# This is where the query with the params is run
+# and then gets all the rows returned by the query and prints them, 
+# or tells you there was nothing found
+    cursor.execute(query, params)
+    results = cursor.fetchall()
+
+    if results:
+        for row in results:
+            print(row)
+    else:
+        print("No matching flights found.")
